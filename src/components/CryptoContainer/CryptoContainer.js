f6b2e1620478ee2956bff01c7eb0d6faa5383d2e
Vladyslav Konovalchuk <berlitio600@gmail.com>
2019-12-29 22:43:15 +0200

    Added List Component

--- src/components/CryptoContainer/CryptoContainer.js
+++ src/components/CryptoContainer/CryptoContainer.js
@@ -1,21 +1,51 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { View, Text } from "react-native";
+import { View, ScrollView } from "react-native";
+import Spinner from "react-native-loading-spinner-overlay";
 import { FetchCoinData } from "../../actions";
-const CryptoContainer = ({}) => {
+import CoinCard from "../CoinCard/CoinCard";
+const CryptoContainer = ({ crypto }) => {
   useEffect(() => {
     FetchCoinData();
-    return () => {
-      ;
-    };
+    return () => {};
   }, []);
+
+  function renderCoins() {
+    return (
+      !!crypto.data.length &&
+      crypto.data.map((coin, idx) => (
+        <CoinCard
+          key={idx}
+          coin_name={coin.name}
+          symbol={coin.symbol}
+          price_usd={coin.price_usd}
+          percent_change_24h={coin.percent_change_24h}
+          percent_change_7d={coin.percent_change_7d}
+        />
+      ))
+    );
+  }
+  function renderSpinner() {
+    return (
+      <Spinner
+        visible={crypto.isLoading}
+        textContent={"Loading...."}
+        animation="fade"
+      />
+    );
+  }
   return (
-    <View>
-      <Text>Crypto List</Text>
-    </View>
+    <ScrollView contentContainerStyle={contentContainer}>
+      {crypto.isLoading ? renderSpinner() : renderCoins()}
+    </ScrollView>
   );
 };
-
+const styles = {
+  contentContainer: {
+    paddingBottom: 100,
+    paddingTop: 55
+  }
+};
 const mapStateToProps = state => ({
   crypto: state.crypto
 });
